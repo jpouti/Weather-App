ef00763e220c0a3e8a11fbee8f25d79f5024534e
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-03-22 23:01:01 +0200

    Add try & catch error block for api call

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,3 +1,5 @@
+import { displayWeather } from "./weather-display";
+
 // store the weather data
 const weatherData = {
     temp: 0,
@@ -10,23 +12,35 @@ const weatherData = {
 // get weather data from api
 const apiData = (() => {
     const metricData = async function(q) {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=43b125624357836ed7034f19487d8f60&units=metric', {mode: 'cors'})
-        const apiData = await response.json();
-        weatherData.temp = apiData.main.temp;
-        weatherData.feels_like = apiData.main.feels_like;
-        weatherData.name = apiData.name;
-        weatherData.country = apiData.sys.country;
-        weatherData.weather = apiData.weather[0].main;
+        try {
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=43b125624357836ed7034f19487d8f60&units=metric', {mode: 'cors'})
+            const apiData = await response.json();
+            weatherData.temp = apiData.main.temp;
+            weatherData.feels_like = apiData.main.feels_like;
+            weatherData.name = apiData.name;
+            weatherData.country = apiData.sys.country;
+            weatherData.weather = apiData.weather[0].main;
+            displayWeather.displayElements();
+        } catch (error) {
+            console.log(error);
+            alert("Location cannot find, please try to search again!");
+        }
         return weatherData;
     }
     const imperialData = async function(q) {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=43b125624357836ed7034f19487d8f60&units=imperial', {mode: 'cors'})
-        const apiData = await response.json();
-        weatherData.temp = apiData.main.temp;
-        weatherData.feels_like = apiData.main.feels_like;
-        weatherData.name = apiData.name;
-        weatherData.country = apiData.sys.country;
-        weatherData.weather = apiData.weather[0].main;
+        try {
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=43b125624357836ed7034f19487d8f60&units=imperial', {mode: 'cors'})
+            const apiData = await response.json();
+            weatherData.temp = apiData.main.temp;
+            weatherData.feels_like = apiData.main.feels_like;
+            weatherData.name = apiData.name;
+            weatherData.country = apiData.sys.country;
+            weatherData.weather = apiData.weather[0].main;
+            displayWeather.displayElements();
+        } catch (error) {
+            console.log(error);
+            alert("Location cannot find, please try to search again!");
+        }
         return weatherData;
     }
     return {metricData, imperialData};
