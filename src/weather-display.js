b73406538b0381daf4444e59a3e2a430c008834d
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-03-24 01:37:46 +0200

    Edit elements -> create elements to for current weather data in js instead html

--- src/weather-display.js
+++ src/weather-display.js
@@ -1,7 +1,10 @@
 import { weatherData } from "./weather-data";
 
+const content = document.querySelector('.content-container');
+
 const displayWeather = (() => {
-    const displayElements = () => {
+    //display current weather
+    const displayCurrentWeather = () => {
         const country = document.querySelector('#country');
         const city = document.querySelector('#city');
         const temp = document.querySelector('#temp');
@@ -13,8 +16,64 @@ const displayWeather = (() => {
         temp.textContent = weatherData.temp;
         feelsLike.textContent = weatherData.feels_like;
         weather.textContent = weatherData.weather;
+    };
+    // create default elements for current weather view
+    const createCurrentWeather = () => {
+        displayWeather.clearElements();
+        const weatherContainer = document.createElement('div');
+        weatherContainer.className = 'weather-container';
+
+        const tempCard = document.createElement('div');
+        tempCard.id = 'temp-card';
+        const temp = document.createElement('p');
+        temp.textContent = "Temperature:";
+        const tempValue = document.createElement('p');
+        tempValue.id = 'temp'
+        tempCard.appendChild(temp);
+        tempCard.appendChild(tempValue);
+
+        const feelsLikeCard = document.createElement('div');
+        feelsLikeCard.id = "feels-like-card";
+        const feelsLike = document.createElement('p');
+        feelsLike.textContent = "Feels like:";
+        const feelsLikeValue = document.createElement('p');
+        feelsLikeValue.id = 'feels-like'
+        feelsLikeCard.appendChild(feelsLike);
+        feelsLikeCard.appendChild(feelsLikeValue);
+
+        const weatherCard = document.createElement('div');
+        weatherCard.id = 'weather-card'
+        const weather = document.createElement('p');
+        weather.textContent = "Weather:";
+        const weatherValue = document.createElement('p');
+        weatherValue.id = 'weather';
+        weatherCard.appendChild(weather);
+        weatherCard.appendChild(weatherValue);
+        
+        const gifCard = document.createElement('div');
+        gifCard.id = 'gif-container'
+        const gif = document.createElement('img');
+        gif.id = 'gif';
+        gifCard.appendChild(gif);
+
+        weatherContainer.appendChild(tempCard);
+        weatherContainer.appendChild(feelsLikeCard);
+        weatherContainer.appendChild(weatherCard);
+        weatherContainer.appendChild(gifCard);
+
+        content.appendChild(weatherContainer);
+    };
+    const clearElements = () => {
+        const element = document.querySelector('.weather-container');
+        if (element != null) {
+            while (element.firstChild) {
+                element.firstChild.remove();
+                element.remove();
+            }
+        }
     }
-    return {displayElements};
+
+    return {displayCurrentWeather, createCurrentWeather, clearElements};
 })();
 
 export {displayWeather};
