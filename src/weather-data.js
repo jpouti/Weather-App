e6618429b3f7ab3759b8cc2eeee6363065fd1537
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-03-23 17:20:35 +0200

    Create src/giphy.js with logic to display gif under the weather data

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,4 +1,5 @@
 import { displayWeather } from "./weather-display";
+import { gif } from "./giphy";
 
 // store the weather data
 const weatherData = {
@@ -15,12 +16,15 @@ const apiData = (() => {
         try {
             const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + q + '&appid=43b125624357836ed7034f19487d8f60&units=metric', {mode: 'cors'})
             const apiData = await response.json();
+            console.log(apiData);
             weatherData.temp = apiData.main.temp;
             weatherData.feels_like = apiData.main.feels_like;
             weatherData.name = apiData.name;
             weatherData.country = apiData.sys.country;
             weatherData.weather = apiData.weather[0].main;
+            let descWeather = apiData.weather[0].description;
             displayWeather.displayElements();
+            gif.displayGif(descWeather);
         } catch (error) {
             console.log(error);
             alert("Location cannot find, please try to search again!");
